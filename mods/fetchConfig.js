4cc5f2a788aec57152ab39da87e3afe6d0450706
pocketmax <sadsdf@adfsadf.com>
2015-01-10 20:32:59 -0700

    fixed #8

--- mods/fetchConfig.js
+++ mods/fetchConfig.js
@@ -1,19 +1,18 @@
-define(function(archCfg, str){
+define(['lodash'], function(_){
 
-    var matchList = archCfg.regex.exec(str);
-    var data = _.clone(archCfg);
-    delete data.cb;
-    delete data.regex;
+    return function(archCfg, str){
+        var matchList = archCfg.regex.exec(str) || [];
+        matchList.shift(0);
 
-    //apply regex match group results to data
-    for(var i in data){
-        data[i] = matchList[i];
+        var data = _.clone(archCfg);
+        delete data.cb;
+        delete data.regex;
+
+        //apply cb results if callbacks present
+        if( _.isFunction( archCfg.cb ) ){
+            data = _.merge(data, archCfg.cb.apply(archCfg, matchList));
+        }
+        return data;
     }
 
-    //apply cb results if callbacks present
-    if( _.isFunction( archCfg.cb ) ){
-        data = _.merge(data, archCfg.cb.apply(archCfg, matchList));
-    }
-
-    return data;
 });
