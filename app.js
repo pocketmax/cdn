4bd43aef9670ea7bddfc105b9bbfb16baaa994a4
pocketmax <sadsdf@adfsadf.com>
2015-02-22 11:58:58 -0700

    fixed #23
    * needs some cleanup

--- app.js
+++ app.js
@@ -4,69 +4,77 @@ var http = require('http'),
     path = require('path'),
     mime = require('mime'),
     request = require('request'),
-    stack = {};
+    redis = require('redis'),
+    client = redis.createClient(6379, 'localdocker');
+
+client.on("error", function (err) {
+	console.log("Error " + err);
+});
+
+// TODO: listen on 'evicted' event to pub/sub to the collection to listen for evicted keys... to delete there package file
+// TODO: cleanup code a bit
 
 var server = http.createServer(function (req, resp) {
 
-    //file already downloaded - stream local file
-    if(stack[req.url]) {
-        console.log('file exists...' + req.url);
-        var rec = stack[req.url];
-        var fileRec = null;
-        var fileHash = null;
-        for(var i in rec.hashes){
-            fileHash = i;
-            fileRec = rec.hashes[i];
-            break;
-        }
+	client.get(req.url, function(err, rec) {
 
-        var filePath = path.join(__dirname + '/files/' + fileHash);
-        var stat = fs.statSync(filePath);
+		if(err) throw err;
+		rec = JSON.parse(rec);
 
-        resp.writeHead(200, {
-            'Content-Type': mime.lookup(filePath),
-            'Content-Length': stat.size
-        });
+		//file already downloaded - stream local file
+		if(rec) {
+			console.log('file exists...');
+			console.log(req.url);
+			console.log(rec);
 
-        var readStream = fs.createReadStream(filePath);
-        readStream.pipe(resp);
+			var filePath = path.join(__dirname + '/files/' + rec.hash);
+			var stat = fs.statSync(filePath);
 
-    //file hasn't been downloaded yet - so return file to client and cache file locally
-    } else {
-        console.log('file is NEW...' + req.url);
+			resp.writeHead(200, {
+				'Content-Type': mime.lookup(filePath),
+				'Content-Length': stat.size
+			});
 
+			var readStream = fs.createReadStream(filePath);
+			readStream.pipe(resp);
 
-        request
-            .get(req.url)
-            .on('response', function(respData){
-                var fileName = respData.headers['Content-Disposition'] || path.basename(req.url);
-                //if we don't get a filename, then don't bother. It's probably not a package to download
-                if(!fileName){
-                    return false;
-                }
-                var tmpFileName = crypto.createHash('md5').update(new Date().toString()).digest('hex');
-                this.pipe(resp);
-                this.pipe(fs.createWriteStream('files/' + tmpFileName));
-                this.on('end', function() {
-                    fs.readFile('files/' + tmpFileName, { encoding: 'utf8' }, function (err, data) {
+		//file hasn't been downloaded yet - so return file to client and cache file locally
+		} else {
 
-                        var hash = crypto.createHash('md5').update(data).digest('hex');
-                        //support /current urls i.e. multiple checksums
-                        var recId = req.url;
-                        var rec = {
-                            hashes: {}
-                        };
-                        rec['hashes'][hash] = {
-                            type: 'md5',
-                            fileName: fileName
-                        };
-                        stack[recId] = rec;
-                        //rename file to hash
-                        fs.renameSync('files/' + tmpFileName, 'files/' + hash);
-                    });
-                });
-            });
-    }
+			console.log('file is NEW...');
+			console.log(req.url);
+
+			request
+			.get(req.url)
+			.on('response', function(respData){
+				var fileName = respData.headers['Content-Disposition'] || path.basename(req.url);
+				//if we don't get a filename, then don't bother. It's probably not a package to download
+				if(!fileName){
+					console.log('had to exit due to no filename');
+					return false;
+				}
+				this.pipe(resp);
+				this.pipe(fs.createWriteStream('tmpfiles/' + fileName));
+				this.on('end', function() {
+					fs.readFile('tmpfiles/' + fileName, { encoding: 'utf8' }, function (err, data) {
+
+						var hash = crypto.createHash('md5').update(data).digest('hex');
+						var rec = {
+							hash: hash,
+							fileName: fileName
+						};
+						console.log('record to be saved...');
+						console.log(rec);
+						client.setex(req.url,100,JSON.stringify(rec));
+						//move file from tmpfiles to files as hash
+						fs.renameSync('tmpfiles/' + fileName, 'files/' + hash);
+					});
+				});
+			});
+		}
+
+	});
+
 });
 
 server.listen(8005, function (req, res) {
